11a279396cbd6586195f808e580ad5500ce2c840
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2019-03-30 03:58:16 +0100

    Update
    Checked, fixed & cleaned.

--- client/esx_joblisting_cl.lua
+++ client/esx_joblisting_cl.lua
@@ -1,5 +1,13 @@
 local Keys = {
-	["ESC"] = 322, ["BACKSPACE"] = 177, ["E"] = 38, ["ENTER"] = 18,	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173
+	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57, 
+	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177, 
+	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70, 
+	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
 }
 
 local menuIsShowed				  = false
@@ -20,11 +28,11 @@ end)
 function ShowProfessionMenu()
 	ESX.UI.Menu.Open("default", GetCurrentResourceName(), "Surgery",
 	{
-	  title = ("Job Center"),
-	  align = "top-left",
+	  title = ("Arbeitsamt"),
+	  align = "bottom-right",
 	  elements = {
-		{label = "Choose profession", value = "choose"},
-		{label = "Apply", value = "apply"}
+		{label = _U('choose_job'),	value = "choose"},
+		{label = _U('apply'), 		value = "apply"}
 	  }
 	},
 	function(data, menu)
@@ -57,8 +65,9 @@ function ShowJobListingMenu(data)
 		ESX.UI.Menu.Open(
 			'default', GetCurrentResourceName(), 'joblisting',
 			{
-				title    = _U('job_center'),
-				elements = elements
+				title    	= _U('job_center'),
+				align 		= "bottom-right",
+				elements 	= elements
 			},
 			function(data, menu)
 				TriggerServerEvent('esx_joblisting:setJob', data.current.value)
@@ -147,7 +156,7 @@ end)
 Citizen.CreateThread(function()
 	for i=1, #Config.Zones, 1 do
 		local blip = AddBlipForCoord(Config.Zones[i].x, Config.Zones[i].y, Config.Zones[i].z)
-		SetBlipSprite (blip, 407)
+		SetBlipSprite (blip, 133)
 		SetBlipDisplay(blip, 4)
 		SetBlipScale  (blip, 1.2)
 		SetBlipColour (blip, 27)
