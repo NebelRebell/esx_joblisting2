1c1134b8cc7b8f49c9a1489083b4c51dfec5d124
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2019-03-30 04:01:17 +0100

    Create
    Translated EN to DE.

---
+++ locales/de.lua
@@ -0,0 +1,14 @@
+Locales['de'] = {
+
+	['new_job'] 			= 'Du hast einen neuen Job!',
+	['access_job_center'] 	= 'Benutze ~INPUT_PICKUP~ um auf das ~b~Arbeitsamt~s~ zuzugreifen.',
+	['job_center'] 			= 'Arbeitsamt',
+	['choose_job'] 			= 'Beruf auswählen',
+	['apply'] 				= 'Bewerben WhitelistJob',
+
+}
+
+-- Translated by NebelRebell
+-- Nebelbank.net
+-- Power of Perfection.
+-- Don't flame without brain!
