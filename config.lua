495080f76714f6da082495e350a6fcd7d697d951
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2019-03-30 04:03:31 +0100

    Update
    Checked, fixed & cleaned.

--- config.lua
+++ config.lua
@@ -1,21 +1,21 @@
 Config              = {}
 Config.DrawDistance = 100.0
-Config.ZoneSize     = {x = 2.0, y = 2.0, z = 2.0}
-Config.MarkerColor  = {r = 0, g = 255, b = 0}
-Config.MarkerType   = 27
-Config.Locale = 'en'
+Config.ZoneSize     = {x = 3.0, y = 3.0, z = 0.5}
+Config.MarkerColor  = {r = 100, g = 100, b = 204}
+Config.MarkerType   = 1
+
+Config.Locale = 'de'
 
 Config.Zones = {
-	{x = -268.8589, y = -956.7532, z = 30.223}
+	{x = -265.00, y = -963.60, z = 30.20}
 }
 
-Config.Webhook = {
-	police = "police",
-	ambulance = "ambulance",
-	journalist = "journalist",
-	cardealer = "cardealer",
-	mechanic = "mechanic",
-	realestate = "realestate",
-	taxi = "taxi",
-	delivery = "delivery"
+Config.Webhook 		= {
+	police 				= "Insert here your Discord Webhook-Link",
+	ambulance 			= "Insert here your Discord Webhook-Link",
+	mechanic 			= "Insert here your Discord Webhook-Link",
+	taxi 				= "Insert here your Discord Webhook-Link",
+	cardealer 			= "Insert here your Discord Webhook-Link",
+	banker 				= "Insert here your Discord Webhook-Link",
+	realestateagent 		= "Insert here your Discord Webhook-Link"
 }
